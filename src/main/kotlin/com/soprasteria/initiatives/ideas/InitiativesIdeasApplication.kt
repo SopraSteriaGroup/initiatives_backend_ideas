04e280fc546994d350ba5c40564bae38cb63257c
jntakpe <jntakpe@gmail.com>
2017-05-13 10:43:18 +0200

    Initialize gradle project

---
+++ src/main/kotlin/com/soprasteria/initiatives/ideas/InitiativesIdeasApplication.kt
@@ -0,0 +1,11 @@
+package com.soprasteria.initiatives.ideas
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class InitiativesIdeasApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(InitiativesIdeasApplication::class.java, *args)
+}
