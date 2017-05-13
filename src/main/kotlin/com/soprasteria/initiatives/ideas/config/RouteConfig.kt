37f49416c5892e7363ce57318c8a2a528a8ddf5f
jntakpe <jntakpe@gmail.com>
2017-05-13 22:00:28 +0200

    #12 Displaying generated docs

--- src/main/kotlin/com/soprasteria/initiatives/ideas/config/RouteConfig.kt
+++ src/main/kotlin/com/soprasteria/initiatives/ideas/config/RouteConfig.kt
@@ -3,8 +3,11 @@ package com.soprasteria.initiatives.ideas.config
 import com.soprasteria.initiatives.ideas.web.IdeasAPI
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.io.ClassPathResource
 import org.springframework.http.MediaType
+import org.springframework.web.reactive.function.server.ServerResponse.temporaryRedirect
 import org.springframework.web.reactive.function.server.router
+import java.net.URI
 
 @Configuration
 class RouteConfig {
@@ -17,5 +20,12 @@ class RouteConfig {
             }
         }
     }
+
+    @Bean
+    fun staticRouter() = router {
+        accept(MediaType.TEXT_HTML).nest { GET("/", { temporaryRedirect(URI.create("html5/index.html")).build() }) }
+        resources("/**", ClassPathResource("static/"))
+    }
+
 
 }
